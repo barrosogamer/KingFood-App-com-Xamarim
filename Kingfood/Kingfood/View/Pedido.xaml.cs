00a4e3a78f3a6a1908e74f3e8f92a62362464475
barrosogamer <gabrielbarreto030@gmail.com>
2020-01-17 00:03:55 -0300

    Adicioando Esbouço e logica de Botoes de +,- e Total

--- Kingfood/Kingfood/View/Pedido.xaml.cs
+++ Kingfood/Kingfood/View/Pedido.xaml.cs
@@ -11,19 +11,18 @@ using Xamarin.Forms.Xaml;
 
 namespace Kingfood.View
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Pedido : INotifyPropertyChanged
-	{
-        
-        public Pedido ()
-		{
-			InitializeComponent ();
-            this.BindingContext = this;
-		}
-       public double qtdCoxinha;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pedido : INotifyPropertyChanged
+    {
 
-      
-       public double QtdCoxinha
+        public Pedido()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+        public double qtdCoxinha;
+
+        public double QtdCoxinha
         {
             get { return qtdCoxinha; }
             set {
@@ -32,17 +31,90 @@ namespace Kingfood.View
                 OnPropertyChanged(nameof(QtdCoxinha));
             }
         }
+        public double qtdEsfirra;
+
+        public double QtdEsfirra
+        {
+            get { return qtdEsfirra; }
+            set
+            {
+                qtdEsfirra = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(QtdEsfirra));
+            }
+        }
+        public double qtdBolinho;
+
+        public double QtdBolinho
+        {
+            get { return qtdBolinho; }
+            set
+            {
+                qtdBolinho = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(QtdBolinho));
+            }
+        }
+        public double qtdKibe;
+
+        public double QtdKibe
+        {
+            get { return qtdKibe; }
+            set
+            {
+                qtdKibe = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(QtdKibe));
+            }
+        }
+
+        private double total;
+
+        
+        public double Total
+        {
+            get { return total; }
+            set { total=value;
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Total));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        }
 
         private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             double value = e.NewValue;
             QtdCoxinha = value;
-
+            Total = QtdBolinho+QtdCoxinha+QtdEsfirra+QtdKibe;
         }
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName]string name="")
+        
+
+        private void Stepper_ValueChanged_1(object sender, ValueChangedEventArgs e)
         {
-            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(name));
+            double value = e.NewValue;
+            QtdEsfirra = value;
+            Total = QtdBolinho + QtdCoxinha + QtdEsfirra + QtdKibe;
+        }
+
+        private void Stepper_ValueChanged_2(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            QtdBolinho = value;
+            Total = QtdBolinho + QtdCoxinha + QtdEsfirra + QtdKibe;
+        }
+
+        private void Stepper_ValueChanged_3(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            QtdKibe = value;
+            Total = QtdBolinho + QtdCoxinha + QtdEsfirra + QtdKibe;
         }
     }
 }
